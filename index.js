ed314b0a9036a628f9469d6dacaef9577056361a
stephen-kovolski <s.kovolski@gmail.com>
2020-05-29 23:41:26 -0400

    cleaned up code

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const PORT = process.env.PORT || 5000
 
 
 app.get('/schools', (req, res) => {
+    res.status(200).json({text: "showing all schools"})
     res.send("home route")
-    res.json({text: "showing all schools"})
 });
 
 app.post('/schools', (req, res) => {
+    res.status(200).json({text: "create new school"})
     res.send("adding a new school")
-    res.json({text: "add a school to the DB"})
 });
 
 app.put('/schools/:id', (req, res) => {
+    res.status(200).json({text: `Update info on an existing school in the DB ${req.params.id}`});
     res.send("update school info")
-    res.json({text: `add to an existing school in the DB ${req.params.id}`});
 });
 
 app.delete('/schools/:id', (req, res) => {
+    res.status(200).json({text: `delete an existing school in the DB ${req.params.id}`});
     res.send("update school info")
-    res.json({text: `delete an existing school in the DB ${req.params.id}`});
 });
 
 
