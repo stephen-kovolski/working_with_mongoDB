66dd76f3b4f210dc4c04b030f39a1a44a7666845
stephen-kovolski <s.kovolski@gmail.com>
2020-06-09 18:27:00 -0400

    dry code

--- methods/escuelas.js
+++ methods/escuelas.js
@@ -1,22 +1,19 @@
 //below are the controlers of each api request.  Whatever the users decideds to do, the directions to complete the request are found in this file in the code below.
 const ErrorResponse = require('../utilities/errorResponse')
+const asyncHandler = require('../middleware/async')
 const Schools = require('../models/Schools')
 
 //Description - Get all schoools
 //Route - GET request to /schools
 //Acess - public
 
-exports.schools = async (req, res, next) => {
+exports.schools = asyncHandler(async (req, res, next) => {
 
-    try {
         const schools = await Schools.find();
 
         res.status(200).json({success: true, count: schools.length, data: schools})
-    } catch (err) {
-        next(err)
-    }
 
-};
+});
 
 
 
@@ -27,21 +24,16 @@ exports.schools = async (req, res, next) => {
 //Route - GET request to /schools/:id
 //Acess - public
 
-exports.getSchoolById = async (req, res, next) => {
+exports.getSchoolById = asyncHandler(async (req, res, next) => {
 
-    try {
         const school = await Schools.findById(req.params.id);
             if (!school) {
               return next(new ErrorResponse(`School not found with id of ${req.params.id}`, 404)); 
 
             }
         res.status(200).json({success: true, data: school})
-    } catch (err) {
-        next(err)
 
-    }
-
-}
+});
 
     
 
@@ -51,28 +43,22 @@ exports.getSchoolById = async (req, res, next) => {
 //Route - POST a new school to /schools
 //Acess - private
 
-exports.createSchool = async (req, res, next) => {
-    try{
+exports.createSchool = asyncHandler(async (req, res, next) => {
     const schools = await Schools.create(req.body);
-
 
     res.send(201).json({
         success: true,
         data: schools
     });
-    } catch (err) {
-        next(err)
-    }   
     
-}
+});
 
 //Description - Update a schoool
 //Route - PUT to /schools:id
 //Acess - private
 
-exports.updateSchool = async (req, res, next) => {
+exports.updateSchool = asyncHandler(async (req, res, next) => {
 
-    try {
 
         const school = await Schools.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
@@ -85,12 +71,8 @@ exports.updateSchool = async (req, res, next) => {
         }
             res.status(200).json({ success: true, data: school})        
         
-    } catch (err) {
-        next(err)
-    }
-
     
-}
+})
 
 
 
@@ -99,9 +81,8 @@ exports.updateSchool = async (req, res, next) => {
 //Route - DELETE a school at route /schools:id
 //Acess - private
 
-exports.deleteSchool =  async (req, res, next) => {
+exports.deleteSchool =  asyncHandler(async (req, res, next) => {
 
-    try {
 
         const school = await Schools.findByIdAndDelete(req.params.id)
     
@@ -110,8 +91,5 @@ exports.deleteSchool =  async (req, res, next) => {
         }
             res.status(200).json({ success: true, data: {}})        
         
-    } catch (err) {
-        next(err)
-    }
-}
+});
 
--- middleware/error.js
+++ middleware/error.js
@@ -7,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
 
     //log to the console for the developer
     //the err has a stack on it which will give us more information, error, file info, etc...
-    console.log(err.stack.gray);
+    console.log(err);
 
 
     //mongoose bad obj id
@@ -15,6 +15,21 @@ const errorHandler = (err, req, res, next) => {
         const message = `School not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404)
     }
+
+    //Mongoose duplicate test.  Im getting the code from th err object - err.code.  Its a unique number to catch. If this code coes up or this error it will display the message below
+    if (err.code === 11000) {
+        const message = 'Duplicate field entered';
+        error = new ErrorResponse(message, 400)
+    }
+
+    //Mongoose validation test
+    if(err.name === 'validationError'){
+        const message = Object.value(err.errors).map(val => val._message);  
+        error = new ErrorResponse(message, 400);
+    }
+
+
+
 
 
     res.status(error.statusCode || 500).json({
@@ -24,4 +39,4 @@ const errorHandler = (err, req, res, next) => {
 }
 
 
-module.exports = errorHandler; 
+module.exports = errorHandler;  
